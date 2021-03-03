328529c081187e89f75b5709469cc56b5b5edadd
Letícia <leticiapyres@gmail.com>
2021-03-03 08:25:46 -0300

    Adicionando arquivos

---
+++ forca.py
@@ -0,0 +1,13 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "banana"
+
+
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
